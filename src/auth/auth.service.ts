b3a74b2c670276a3f549d4badbf46cc364880aad
unknown <tranbamanhlovevietnam@gmail.com>
2023-07-11 09:45:04 +0700

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class AuthService {}
+@Injectable({})
+export class AuthService {
+  login() {
+    return 'Login!';
+  }
+  register() {
+    return 'Register !';
+  }
+}
